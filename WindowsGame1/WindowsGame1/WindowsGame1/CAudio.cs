97f8b32703fab35ae553a584adbc8d2843c5f456
Ross Robertson <rossrobertsonjnr@msn.com>
2012-01-27 21:42:13 +0000

    First launch

---
+++ WindowsGame1/WindowsGame1/WindowsGame1/CAudio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    class CAudio
+    {
+        Song backgroundMusic;
+        SoundEffect effect;
+
+        public void init(ContentManager content, String filename)
+        {
+            backgroundMusic = content.Load<Song>(filename);
+            MediaPlayer.Play(backgroundMusic);
+            MediaPlayer.IsRepeating = true;
+        }
+
+        public void playEffect(String soundname, ContentManager content, bool sound)
+        {
+            if (sound == true)
+            {
+                effect = content.Load<SoundEffect>(soundname);
+                effect.Play();
+            }
+        }
+
+        public void backgroundMusicToggle()
+        {
+            if (MediaPlayer.State == MediaState.Playing)
+            {
+                MediaPlayer.Pause();
+            }
+            else
+            {
+                MediaPlayer.Resume();
+            }
+        }
+    }
+}
---
+++ WindowsGame1/WindowsGame1/WindowsGame1/CSprite.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace WindowsGame1
+{
+    class CSprite
+    {
+        Vector3 m_Pos; //position
+        Vector3 m_Dir; //movement direction
+        Texture2D m_Texture; //texture of sprite
+        Rectangle m_Rect; //rectangle used for drawing sprite
+        /// <summary>
+        /// 
+        /// </summary>
+        public CSprite() //general constructor
+        {
+            m_Pos = new Vector3(0, 0, 0); //set position null
+            m_Dir = new Vector3(0, 0, 0); //set direction null
+        }
+
+        public void createSprite(ContentManager content, Vector3 position, String texName)
+        {
+            m_Pos = position; //set position to user input
+            m_Texture = content.Load<Texture2D>(texName); //set texture to texture name
+        }
+        
+        public void update()
+        {
+            //m_Pos += m_Dir; //move position with regards to direction
+        }
+        
+        public void draw(SpriteBatch sb)
+        {
+            m_Rect = new Rectangle((int) m_Pos.X, (int) m_Pos.Y, m_Texture.Width, m_Texture.Height);
+            sb.Draw(m_Texture, m_Rect, Color.White);
+        }
+
+        
+    }
+}
